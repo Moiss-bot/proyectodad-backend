23fc6a283ec6919731151d303572c00297d0372d
Lucky-pixel-web <luckyinever23@gmail.com>
2026-06-21 12:23:03 -0500

    Commit 2

--- ms-proveedores/src/main/java/pe/edu/upeu/msproveedores/controller/ProveedorController.java
+++ ms-proveedores/src/main/java/pe/edu/upeu/msproveedores/controller/ProveedorController.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/proveedores")
-
 
 public class ProveedorController {
 
